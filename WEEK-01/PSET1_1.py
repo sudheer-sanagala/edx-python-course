1e357ddd3a0db062c1c9052021bcf9b3370bc976
Sudheer <Sudheer.Sanagala@specialized.com>
2019-09-06 23:40:49 -0700

    Test solution added for 1 & 2

--- WEEK-01/PSET1_1.py
+++ WEEK-01/PSET1_1.py
@@ -5,3 +5,13 @@ Write a program that counts up the number of vowels contained in the string s. V
 For example, if s = 'azcbobobegghakl', your program should print:
 Number of vowels: 5
 """
+
+s = 'azcbobobegghakl'
+
+vowel = 0
+
+for x in s:
+    if (x == 'a' or x == 'e' or x == 'i' or x == 'o' or x == 'u'):
+        vowel = vowel + 1
+
+print('Number of vowels: ', vowel)
--- WEEK-01/PSET1_2.py
+++ WEEK-01/PSET1_2.py
@@ -5,3 +5,18 @@ Write a program that prints the number of times the string 'bob' occurs in s.
 For example, if s = 'azcbobobegghakl', then your program should print 
 Number of times bob occurs is: 2
 """
+
+s = 'azcbobobegghakl'
+check_str = 'bob'
+count = 0
+
+# First check if given string is greater than the matching word
+if len(s) >= len(check_str):
+
+    # now traverse the entire length of the string to find 3 digit word
+    for i in range(len(s)):
+
+        # now check if the next 3 elements are equal to matched string
+        if s[i:i+3] == check_str:
+            count = count+1
+    print('Number of times bob occurs is: ', count)
